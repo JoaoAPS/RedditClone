3ff407412b64c24ee7a59c2067e46b4fbaedd748
João Paludo <jas.paludo@gmail.com>
2024-03-18 21:38:16 -0300

    Use Turso for DB

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,6 +1,8 @@
-import { drizzle } from 'drizzle-orm/libsql';
-import { createClient } from '@libsql/client';
+import { drizzle } from "drizzle-orm/libsql"
+import { createClient } from "@libsql/client"
 
 import { env } from "~/env"
 
-export const db = drizzle(createClient({ url: env.DATABASE_URL }));
+export const db = drizzle(
+  createClient({ url: env.DATABASE_URL, authToken: env.DATABASE_AUTH_TOKEN }),
+)
